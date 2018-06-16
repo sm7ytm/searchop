8a327661ebd4db5774e5bddacabb7f0d606405b8
Magnus Jönsson <sm7ytm@gmail.com>
2018-06-16 16:50:13 +0200

    Ny fil med funktionerna för operatörssökning och tillgänglighet sammanslagna
    Skrivit ny fil för att strukturera funktionerna och ta bort utskrifter
    i funktionerna samt slå samman de två sökningarna och få ett gemensamt
    resultat istället. En fråga ger svar både på operatören och om numret
    är tillgängligt

---
+++ searchop.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+""" Programmet kollar ett telefonnummer hos PTS huruvida det är ledigt eller vilken operatör det tillhör"""
+
+import urllib2
+from screen_util import clear       # Egen modul för att rensa skärmen
+
+
+def search_op_by_number(nummer):        # Kolla vilken operatör som har numrettry:
+
+    try:
+        webbsida = urllib2.urlopen('http://api.pts.se/PTSNumberService/Pts_Number_Service.svc/pox/SearchByNumber?number=' + str(nummer))    # Öppna sidan och klistra ihop adressen med isbn-numret
+        data = webbsida.read()
+        return data
+
+    except urllib2.HTTPError:
+        clear()
+        print '\n\tNågot blev fel, troligen hittades inte numret du sökte\n\n'
+
+
+def is_available(nummer):           # Funktion för att kolla om ett specifikt nummer är ledigt
+
+    try:
+        webbsida = urllib2.urlopen('http://api.pts.se/PTSNumberService/Pts_Number_Service.svc/pox/IsNumberAvailable?number=' + str(nummer))
+        data = webbsida.read()
+
+        start = data.find('">') + 2
+        slut = data.find('</Int>') - 5
+
+        if data[start:slut] == '0':     # En nolla i retur visar att numret är ledigt
+            return 'Ledigt'
+        elif data[start:slut] == '1':
+            return 'Upptaget'   # och en etta så är det upptaget
+        else:
+            return 'Fel'
+
+    except urllib2.HTTPError:
+        clear()
+        print '\n\tNågot blev fel\n\n'
+
+
+clear()
+tfnum = raw_input('\tSök telefonnummer (format xxxx-xxxxxx el. xxx-xxxxxxx) : ')                # Fråga efter önskat nummer
+nummer = tfnum[1:]
+
+op_data = search_op_by_number(nummer)
+op_start = op_data.find('<Name>') + 6       # Plocka ut start-index på texten
+op_slut = op_data.find('</Name>')           # och slut index
+
+ledigt = is_available(nummer)
+
+clear()
+print '\tOperatörsfråga\n'
+print '\tNummer          : %s' % tfnum
+print '\tOperatör        : ' + op_data[op_start:op_slut]
+print '\tTillgänglighet  : %s' % ledigt
+print '\n' * 3
